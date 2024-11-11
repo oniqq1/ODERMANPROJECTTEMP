feeefef3e49613e5a55d96a787d5a1da7c55dfcf
ONIQQ <insolencem1.1@gmail.com>
2024-11-11 11:38:27 +0200

    check commit

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 import sqlite3
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='temp')
 
 name = "Арсен"
 surname = "Кропачев"
